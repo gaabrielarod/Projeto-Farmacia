59667994386f6c30b08c9bdbdaf57cdca77928ed
gaabrielarod <gabrielasouzarod@gmail.com>
2023-05-23 10:30:07 -0300

    First commit

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,75 @@
+package com.generation.farmacia.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "Campo obrigatório")
+	@Size(min = 3, max = 100, message = "Este campo deve ter no mínimo 3 caracteres e no máximo  100 caracteres")
+	private String nome;
+	
+	@Positive(message = "Campo não pode ter valor negativo")
+	@NotNull(message = "Campo obrigatório")
+	private Integer preco;
+	
+	@ManyToOne 
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Integer preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+	
+	
+	
+
+}
